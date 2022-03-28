be56840d354226b2f94e3835e24c090c3414b7a6
murilinhops <murilo.pescatori@nubank.com.br>
2022-03-28 16:26:40 -0300

    Create constants values and Strings

---
+++ lib/constants.dart
@@ -0,0 +1,59 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xff2bc089);
+
+const socialLinks = [
+  'https://instagram.com/murilinhops',
+  'https://twitter.com/PsMurilinho',
+  'https://linkedin.com/in/murilinhops',
+  'https://github.com/murilinhoPs',
+];
+
+const kTools = [
+  'Flutter',
+  'Dart',
+  'C++',
+  'Unreal Engine',
+  'C#',
+  'Unity',
+  'JavaScript',
+  'React',
+  'Clojure',
+];
+
+const projectsBanner = [
+  'assets/images/bloor_runner.png',
+  'assets/images/brackeys_jam.png',
+  'assets/images/pokedex.png',
+  'assets/images/flutter_blog.png',
+  'assets/images/ben_and_buzz.png',
+  'assets/images/nike.png',
+  'assets/images/cyber_rider.png',
+];
+const projectsTitles = [
+  'Blood Runner',
+  'Without Title',
+  'Pokedex',
+  'Flutter Blog',
+  'Ben and Buzz',
+  'Nike Shop UI',
+  'Cyber Rider',
+];
+
+const projectsLinks = [
+  'https://galadrielf.itch.io/bloodrunner',
+  'https://murilinps.itch.io/without-title',
+  'https://github.com/murilinhoPs/minha_pokedex',
+  'https://github.com/murilinhoPs/flutter_social_blog',
+  'https://gnitolo.itch.io/ben-buzz',
+  'https://github.com/murilinhoPs/challenge_nike_app',
+  'https://gamejolt.com/games/cyberrider/446167'
+];
+
+const emailTitle = 'Email';
+const email = 'murilinhorps@gmail.com';
+
+const resumeLinks = [
+  'https://drive.google.com/file/d/1MlwslHMYIaiUhCvATxDOPrzSgINZDIAo/view?usp=sharing', //en-us
+  'https://drive.google.com/file/d/11k6hLupqL1_ObtB-gVECG0xpzSfjE3xe/view?usp=sharing', //pt-br
+];
---
+++ lib/strings.dart
@@ -0,0 +1,39 @@
+class Strings {
+  static const projectsDescription = [
+    'A runner game made in Unreal Engine about Bloodborne for summer slow game jam, which the theme was Remix games and Mods.',
+    'A 2D shooting game made in Unity about a Kid imagining shadows in the house. It was for Brackeys Game Jam 2022, with Its Not Real theme',
+    'A pokedex app in flutter with clean architecture and BLOC using pokeApi',
+    'Social blogging app inspired by twitter but with halloween theme, and the backend was made in NodeJs with Typescript',
+    'Ben & Buzz is a 3D game made in Unity, to bring representativeness through a PwD protagonist with Duchenne Muscular Dystrophy.',
+    'A design and animations UI challenge in flutter by Flutterando Challenge inspired by Nike theme',
+    'This is a casual game, with cyberpunk theme and as reference I used the game rider for mobile too.'
+        'This is a casual endless runner game with Cyberpunk theme I made in Unity. The game Rider for android and ios was used as a reference',
+  ];
+
+  static const location = 'Location';
+  static const city = 'São Paulo, Brazil';
+  static const myName = 'Murilo Pescatori Souza';
+  static const myNickname = 'murilinho';
+
+  static const aboutNavBar = 'About';
+  static const homeNavBar = 'Home';
+  static const projectsNavBar = 'Projects';
+  static const contactNavBar = 'Contact';
+  static const nameNavBar = 'Murilo Ps';
+
+  static const aboutTitle = 'About';
+  static const homeTitle = 'Home';
+  static const projectsTitle = 'Recent Works';
+  static const contactTitle = 'Get In Touch';
+  static const madeWithFlutter = 'Made with flutter 💙';
+  static const projectsSubtitle = r'''Some projects I've done so far''';
+
+  static const homeSubtitle = r'''Hi! I'm Murilo Ps.''';
+  static const homeMiniBio =
+      r'''I'm a Game Designer with focus on programming games using Unity and Unreal Engine and I'm always learning how to improve my games. I also make mobile apps in Flutter, and I'm working with Flutter now''';
+
+  static const aboutMeDescription =
+      r'''I´m Murilo Pescatori Souza, 22 years old and a PwD with Duchenne Muscular Dystrophy. I have a bachelor's degree in Game Design and now I'm working at a Nubank as Mobile developer. Since I was a kid I wanted to learn about Game Development, how games are made. And now I´m into this marvelous world! 
+My focus is on programming, and my goal is to be a very good programmer. During the development of a project I´m constantly learning how to improve them and I think the best way of doing this is testing with users and my friends to get feedback, this helps me on which way to improve the interactions and mechanics.
+Even though my area is coding, is important knowing all the process and principles of making games. Understanding all the process makes easier to get the feedback, and as a programmer you have to make the interaction works and how to fix it if it´s not working propperly, understanding the interaction makes easier to improve it by coding.''';
+}
