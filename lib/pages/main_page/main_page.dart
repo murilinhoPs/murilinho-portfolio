a23839a4a4d65645eb9fc7ff42b0bfa64678ccea
murilinhops <murilo.pescatori@nubank.com.br>
2022-03-29 18:03:06 -0300

    Add Home section to MainPage

--- lib/pages/main_page/main_page.dart
+++ lib/pages/main_page/main_page.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:murilinho_portfolio/pages/main_page/sections/home/home_section.dart';
 import 'package:murilinho_portfolio/pages/main_page/sections/nav_bar/nav_bar.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           NavBar(),
           Expanded(
-            child: Center(
-              child: Text('Main'),
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              children: [
+                SizedBox(height: 20.0),
+                HomeSection(),
+              ],
             ),
           ),
         ],
