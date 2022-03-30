291c024f29ae0d52ed61c282bf47d45a0ddbd167
murilinhops <murilinhorps@gmail.com>
2022-03-30 20:38:55 -0300

    Add brackeys game jam picture

--- lib/assets_path.dart
+++ lib/assets_path.dart
@@ -1,4 +1,6 @@
 class Assets {
   static const meCartoon = 'assets/images/me_cartoon.jpeg';
   static const mePortrait = 'assets/images/eu_retrato.png';
+
+  static const brackeysJam = 'assets/images/brackeys_jam.png';
 }
