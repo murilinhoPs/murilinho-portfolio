43f2cd29a2dbecbe45b94242ae30a14e23e735d9
murilinhops <murilinhorps@gmail.com>
2022-04-05 20:58:41 -0300

    Create about section

--- lib/pages/main_page/sections/about/about_section_desktop.dart
+++ lib/pages/main_page/sections/about/about_section_desktop.dart
@@ -1,10 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:murilinho_portfolio/utils/assets_path.dart';
+import 'package:murilinho_portfolio/utils/constants.dart';
+import 'package:murilinho_portfolio/utils/helpers.dart';
+import 'package:murilinho_portfolio/utils/strings.dart';
 
 class AboutSectionDesktop extends StatelessWidget {
   const AboutSectionDesktop({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text('Desktop');
+    final experiences = experiencesAbout
+        .map(
+          (exp) => Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4.0,
+            ),
+            child: Text('$exp,'),
+          ),
+        )
+        .toList();
+
+    return Column(
+      children: [
+        Text(
+          Strings.aboutTitle,
+          style: TextStyle(
+            fontSize: 42,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.6,
+          ),
+        ),
+        SizedBox(height: 16.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              Assets.meImgFull,
+              height: 320,
+            ),
+            SizedBox(
+              width: 80,
+            ),
+            Expanded(
+              child: Text(
+                Strings.aboutMeDescription,
+                softWrap: true,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  height: 1.6,
+                  letterSpacing: 1.2,
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 12.0),
+        Divider(color: Colors.grey[600]),
+        SizedBox(height: 12.0),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              Strings.aboutMeExperience,
+              style: TextStyle(
+                fontSize: 20,
+                decoration: TextDecoration.underline,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(width: 12.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: experiences,
+            ),
+          ],
+        ),
+        SizedBox(height: 24.0),
+        MaterialButton(
+          hoverColor: primaryColor.withAlpha(150),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            side: BorderSide(
+              color: primaryColor,
+              width: 2.0,
+            ),
+          ),
+          padding: EdgeInsets.all(12.0),
+          onPressed: () => launchURL(resumeLink),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 4.0,
+            ),
+            child: Text(Strings.resume),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/pages/main_page/sections/about/about_section_mobile.dart
+++ lib/pages/main_page/sections/about/about_section_mobile.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:murilinho_portfolio/utils/assets_path.dart';
+import 'package:murilinho_portfolio/utils/constants.dart';
+import 'package:murilinho_portfolio/utils/helpers.dart';
+import 'package:murilinho_portfolio/utils/strings.dart';
 
 class AboutSectionMobile extends StatelessWidget {
   const AboutSectionMobile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text('Mobile');
+    final experiences = experiencesAbout
+        .map(
+          (exp) => Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4.0,
+            ),
+            child: Text('$exp,'),
+          ),
+        )
+        .toList();
+
+    return Column(
+      children: [
+        Text(
+          Strings.aboutTitle,
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.6,
+          ),
+        ),
+        SizedBox(height: 16.0),
+        Image.asset(
+          Assets.meImgFull,
+          height: 280,
+        ),
+        SizedBox(
+          height: 32,
+        ),
+        Text(
+          Strings.aboutMeDescription,
+          softWrap: true,
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            height: 1.2,
+          ),
+        ),
+        SizedBox(height: 12.0),
+        Divider(color: Colors.grey[600]),
+        SizedBox(height: 12.0),
+        Wrap(
+          crossAxisAlignment: WrapCrossAlignment.start,
+          runSpacing: 12,
+          spacing: 12,
+          children: [
+            Text(
+              Strings.aboutMeExperience,
+              style: TextStyle(
+                fontSize: 16,
+                decoration: TextDecoration.underline,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(width: 12.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: experiences,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 24.0),
+        MaterialButton(
+          hoverColor: primaryColor.withAlpha(150),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            side: BorderSide(
+              color: primaryColor,
+              width: 2.0,
+            ),
+          ),
+          padding: EdgeInsets.all(12.0),
+          onPressed: () => launchURL(resumeLink),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 4.0,
+            ),
+            child: Text(Strings.resume),
+          ),
+        ),
+      ],
+    );
   }
 }
