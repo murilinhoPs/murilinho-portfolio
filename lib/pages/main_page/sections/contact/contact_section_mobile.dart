d98bfa91e009663139e2a5dcbd1fbbb0a9af11e0
murilinhops <murilo.pescatori@nubank.com.br>
2022-04-06 15:10:20 -0300

    Remove unused padding

--- lib/pages/main_page/sections/contact/contact_section_mobile.dart
+++ lib/pages/main_page/sections/contact/contact_section_mobile.dart
@@ -9,35 +9,30 @@ class ContactSectionMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            Strings.contactTitle,
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.6,
-            ),
+    return Column(
+      children: [
+        Text(
+          Strings.contactTitle,
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.6,
           ),
-          ContactInfoWidget(
-            title: Strings.location,
-            info: Strings.city,
-          ),
-          ContactInfoWidget(
-            title: emailTitle,
-            info: emailLink,
-          ),
-          ContactInfoWidget(
-            title: linkedinTitle,
-            info: linkedinLink,
-            onTap: () => launchURL(linkedinLink),
-          ),
-        ],
-      ),
+        ),
+        ContactInfoWidget(
+          title: Strings.location,
+          info: Strings.city,
+        ),
+        ContactInfoWidget(
+          title: emailTitle,
+          info: emailLink,
+        ),
+        ContactInfoWidget(
+          title: linkedinTitle,
+          info: linkedinLink,
+          onTap: () => launchURL(linkedinLink),
+        ),
+      ],
     );
   }
 }
